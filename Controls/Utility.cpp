0ef5feb0224296817fd86805e8d586a045c69a4f
Matteo Baratella <matteo.baratella96@mail.com>
2020-07-02 22:44:16 +0200

    Updated Models, Added Controls folder with Parser and Utility, added policy folder

---
+++ Controls/Utility.cpp
@@ -0,0 +1,38 @@
+//
+// Created by mbaratella on 02/07/2020.
+//
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
+
+class Utility {
+public:
+    /***
+     * Split a string into an array of substrings, and returns the new array
+     * @param line: string to split
+     * @param delimiter: Specifies the character to use for splitting the string
+     * @return An Array, containing the splitted values
+     */
+    static vector<string> split(const string& line, char delimiter) {
+        istringstream iss(line);
+        vector<string> values;
+        string s;
+        while (getline( iss, s, delimiter ) ) {
+            values.push_back(s);
+        }
+        return values;
+    }
+
+    /***
+     * Print an array of string
+     * @param vector
+     */
+    static void printVector(const vector<string>& vector) {
+        for (auto & value : vector)
+            cout << value << " ";
+    }
+
+};
