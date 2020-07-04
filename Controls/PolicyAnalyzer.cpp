c8c9d83bb03f2d752a0993a3e4a043c359679f60
andi arapi <andi_arapi@hotmail.it>
2020-07-04 16:49:18 +0200

    backwardSlicing update

--- Controls/PolicyAnalyzer.cpp
+++ Controls/PolicyAnalyzer.cpp
@@ -5,6 +5,7 @@
 
 #include <vector> //std::vector
 #include "../Models/Policy/Policy.h"
+
 using namespace std;
 
 class PolicyAnalizer {
@@ -14,22 +15,22 @@ template <class T>
     vector<T> updatePolicy(vector<T>  v1, vector<string> & v2){
         vector<T> aux;
         for (T x : v1)
-            if (find(v2,x.getRoleAdmin()))
+            if (Utility::findRole(x.getRoleAdmin(),v2))
                 aux.push_back(x);
         return aux;
     }
 
     Policy & backwardSlicing(Policy & policy){
-        vector<string> s;
-        s.push_back(policy.getGoal());
+        vector<string> S;
+        S.push_back(policy.getGoal());
         for (int i=0;i<100;++i){
-            for (CA x : policy.getCanAssign())
-               if(find(s,x.getRoleToAssign())){
-                   s.push_back(x.getRoleAdmin());
-                   for (string role: x.getNegativeConditions())
-                       s.push_back(role);
-                   for (string role: x.getPositiveConditions())
-                       s.push_back(role);
+            for (CA ca : policy.getCanAssign())
+               if(empty(Utility::findRole(ca.getRoleToAssign(),S))){
+                   S.push_back(ca.getRoleAdmin());
+                   for (string r: ca.getNegativeConditions())
+                       S.push_back(r);
+                   for (string r: ca.getPositiveConditions())
+                       S.push_back(r);
                }
         }
         policy.setCanAssign(updatePolicy<CA>(policy.getCanAssign(), s));
