8eb74d310b98d0abaff663287691f193167fa9bf
Matteo Baratella <matteo.baratella96@mail.com>
2020-09-12 14:54:44 +0200

    Added time exec log

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include "Controls/Parser.cpp"
 #include "Controls/PolicyAnalyzer.cpp"
+#include <ctime>
 using namespace std;
 
 int main() {
+    clock_t start = clock();
     bool showLogs = true;
     bool enableCache = true;
+    bool showExecTime = false;
     string filename = "../policies/policy1.arbac";
     Policy policy;
 
@@ -23,6 +26,11 @@ int main() {
     }
     catch (const char* msg) {
         cerr << msg << endl;
+    }
+    clock_t stop = clock();
+    if(showExecTime) {
+        double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
+        cout << "Time elapsed: " << elapsed << endl;
     }
     return 0;
 }
